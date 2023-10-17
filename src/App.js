188e6161404274bf0903593026671b2d40c4634e
Pavan Chintakayala <pa1chintakayala@gmail.com>
2023-10-17 11:55:37 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import './App.css'
+import AdminCourse from './components/AdminCourses/AdminCourse';
+import EditCourse from "./components/AdminCourses/EditCourse";
 import Course from './components/Courses/course';
+
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
@@ -19,6 +22,9 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/courses' element={<Course/>}/>
+            <Route path='/admincourses' element={<AdminCourse/>}/>
+            <Route path='/editcourse/:id' element={<EditCourse />}></Route>
+
           </Routes>
         </div>
 
