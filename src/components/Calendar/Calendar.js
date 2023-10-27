0a2898212964d3f4fdc6ace61d4364fd9a603dad
Mohankrish007 <mohangarnepudi2001@gmail.com>
2023-10-27 14:48:44 +0530

    calendar updated for adding events for admin

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calendar.css'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
 import format from 'date-fns/format'
@@ -7,6 +7,9 @@ import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
 
 const locales = {
   'en-US': enUS,
@@ -43,14 +46,53 @@ const events = [
 
 
 function Calender() {
+
+  const [isAdmin, setIsAdmin] = useState(true)
+  const [allEvents, setAllEvents] = useState(events)
+  const [newEvent, setNewEvent] = useState({
+    title:"",
+    start:"",
+    end:""
+  })
+
+  const handleAddEvent = () =>{
+
+    const formattedStart = newEvent.start instanceof Date ? newEvent.start : parse(newEvent.start, 'yyyy-MM-dd', new Date());
+    let formattedEnd = newEvent.end instanceof Date ? newEvent.end : parse(newEvent.end, 'yyyy-MM-dd', new Date());
+    formattedEnd = new Date(formattedEnd.getTime() + 24 * 60 * 60 * 1000);
+    setAllEvents([...allEvents, { ...newEvent, start: formattedStart, end: formattedEnd }]);
+
+  }
+
   return (
     <div>
+      
+      {isAdmin &&
+        <div className='add-events-admin'>
+          <div className='add-events-header'>Add Event</div>
+          <div className='add-events-inputs'>
+            <div className='add-event-dates'>
+              <div>Title :</div>
+              <input className='add-events-title' type="text" placeholder='Add event title..' value={newEvent.title} onChange={(e)=>setNewEvent({...newEvent, title:e.target.value})}/>
+            </div>
+            <div className='add-event-dates'>
+              <div>Start date :</div>
+              <DatePicker value={newEvent.start} onChange={(start) => setNewEvent({...newEvent,start})}/>
+            </div>
+            <div className='add-event-dates'>
+              <div>End date :</div>
+              <DatePicker value={newEvent.end} onChange={(end) => setNewEvent({...newEvent,end})}/>
+            </div>
+            <button className='add-event-button' onClick={handleAddEvent}>Add event</button>
+          </div> 
+      </div>}
+
         <Calendar
       localizer={localizer}
-      events={events}
+      events={allEvents}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
+      style={{ height: 500 , }}
     />
     </div>
   )
