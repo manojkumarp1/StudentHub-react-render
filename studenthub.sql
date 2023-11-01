d22b4fcbbbeef4683413cbfe4427b1f0ab2b0789
Pavan Chintakayala <pa1chintakayala@gmail.com>
2023-11-01 12:22:42 +0530

    sql file

--- studenthub.sql
+++ studenthub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2023 at 05:59 AM
+-- Generation Time: Nov 01, 2023 at 07:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -67,6 +67,28 @@ INSERT INTO `courses` (`id`, `coursename`, `imgurl`, `description`, `duration`) 
 (6, 'Dotnet', 'https://www.srishticampus.com/packageImages/dotnet_round.png', 'Harness the strength of a unified platform', '12 hours'),
 (7, 'Javascript', 'https://miro.medium.com/v2/resize:fit:828/format:webp/1*98ydKJQSWDGwHZxUV_-kLw.png', 'Ignite your web creations ', '10 hours'),
 (12, 'Javascript', 'https://miro.medium.com/v2/resize:fit:828/format:webp/1*98ydKJQSWDGwHZxUV_-kLw.png', 'Ignite your web creations ', '10 hours');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enroll`
+--
+
+CREATE TABLE `enroll` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `selectedcourse` varchar(250) NOT NULL,
+  `duration` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `enroll`
+--
+
+INSERT INTO `enroll` (`id`, `name`, `selectedcourse`, `duration`) VALUES
+(1, 'ashok raj', 'Python Programming Language', '10 hours'),
+(2, 'pavan hi', 'Python Programming Language', '10 hours'),
+(3, 'ashok raj', 'Dotnet', '12 hours');
 
 -- --------------------------------------------------------
 
@@ -135,11 +157,12 @@ CREATE TABLE `students` (
 
 INSERT INTO `students` (`id`, `firstname`, `lastname`, `dob`, `email`, `phone`, `password`, `profilePic`) VALUES
 (1, 'hi', 'hi', '0000-00-00', 'hi@gmail.com', 'hi', 'hi@gmail.com', NULL),
-(2, 'PavanChintakayala', 'PavanChintakayala', '0000-00-00', 'PavanChintakayala@gmail.com', '1234567890', 'PavanChintakayala@gm', NULL),
 (3, 'admin123', 'admin123', '0000-00-00', 'admin123@gmail.com', '1234567890', 'Pavan@180602', NULL),
 (4, 'ram', 'ram', '0000-00-00', 'ram@gmail.com', '9874561235', 'ram', NULL),
 (5, 'ashok', 'raj', '2023-10-06', 'raj@gmail.com', '9874563212', 'rajrajraj', '/uploads/apartment.jpg'),
-(6, 'admin', 'admin', '2023-10-06', 'admin@gmail.com', '9876543213', 'admin@gmail.com', NULL);
+(6, 'admin', 'admin', '2023-10-06', 'admin@gmail.com', '9876543213', 'admin@gmail.com', NULL),
+(7, 'Pavan', 'Chintakayala', '2023-11-16', 'pa1chintakayala@gmail.com', '7671831838', 'pa1chintakayala@gmai', NULL),
+(8, 'Empty', 'Empty', '2023-11-13', 'empty@gmail.com', '1234567890', '1234567890', NULL);
 
 --
 -- Indexes for dumped tables
@@ -155,6 +178,12 @@ ALTER TABLE `admin`
 -- Indexes for table `courses`
 --
 ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `enroll`
+--
+ALTER TABLE `enroll`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -192,6 +221,12 @@ ALTER TABLE `courses`
   MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
+-- AUTO_INCREMENT for table `enroll`
+--
+ALTER TABLE `enroll`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
@@ -207,7 +242,7 @@ ALTER TABLE `problems`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
