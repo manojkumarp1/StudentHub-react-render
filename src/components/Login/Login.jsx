8d59a41f15ad649bad8859fb3a2b53f1d617f197
ganeshkrishna1 <ganeshkrishnapagadala@gmail.com>
2023-12-04 08:30:07 +0530

    ok

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
       localStorage.setItem('authenticatedUser', false);
       localStorage.setItem('authenticatedAdmin', true);
       navigate('/admincourses');
-      windows.reload(true);
+      window.location.reload(true);
     } else {
 
       // Make an API request to the login endpoint without validation
@@ -43,7 +43,7 @@ function Login() {
             localStorage.setItem('studentId', studentId); // Store userId in localStorage
             setstudentId(studentId); 
             navigate('/home', { replace: true }); // Navigate back to the home page without a full page reload
-            windows.reload(true);
+            window.location.reload(true);
           } else {
             navigate('/signup');
           }
